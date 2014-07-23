12ef5986e70f48ad5efca425fd2d3a09e9a8130c
Francisco J. Fernandez <fran@jobandtalent.com>
2014-07-23 19:03:44 +0200

    Simplified NetworkBitmapHunter to have a bigger limit for streaming buffer and decoding just bytearrays. There was a problem with skia, decoder and so.

--- picasso/src/main/java/com/squareup/picasso/NetworkBitmapHunter.java
+++ picasso/src/main/java/com/squareup/picasso/NetworkBitmapHunter.java
@@ -29,7 +29,7 @@ import static com.squareup.picasso.Utils.hasPermission;
 
 class NetworkBitmapHunter extends BitmapHunter {
   static final int DEFAULT_RETRY_COUNT = 2;
-  private static final int MARKER = 65536;
+  private static final int MARKER = 2097152;
 
   private final Downloader downloader;
 
@@ -98,38 +98,20 @@ class NetworkBitmapHunter extends BitmapHunter {
   }
 
   private Bitmap decodeStream(InputStream stream, Request data) throws IOException {
-    MarkableInputStream markStream = new MarkableInputStream(stream);
-    stream = markStream;
+      MarkableInputStream markStream = new MarkableInputStream(stream);
+      stream = markStream;
 
-    long mark = markStream.savePosition(MARKER);
+      long mark = markStream.savePosition(MARKER);
 
-    final BitmapFactory.Options options = createBitmapOptions(data);
-    final boolean calculateSize = requiresInSampleSize(options);
+      final BitmapFactory.Options options = createBitmapOptions(data);
+      final boolean calculateSize = requiresInSampleSize(options);
 
-    boolean isWebPFile = Utils.isWebPFile(stream);
-    markStream.reset(mark);
-    // When decode WebP network stream, BitmapFactory throw JNI Exception and make app crash.
-    // Decode byte array instead
-    if (isWebPFile) {
+      markStream.reset(mark);
       byte[] bytes = Utils.toByteArray(stream);
       if (calculateSize) {
-        BitmapFactory.decodeByteArray(bytes, 0, bytes.length, options);
-        calculateInSampleSize(data.targetWidth, data.targetHeight, options);
+          BitmapFactory.decodeByteArray(bytes, 0, bytes.length, options);
+          calculateInSampleSize(data.targetWidth, data.targetHeight, options);
       }
       return BitmapFactory.decodeByteArray(bytes, 0, bytes.length, options);
-    } else {
-      if (calculateSize) {
-        BitmapFactory.decodeStream(stream, null, options);
-        calculateInSampleSize(data.targetWidth, data.targetHeight, options);
-
-        markStream.reset(mark);
-      }
-      Bitmap bitmap = BitmapFactory.decodeStream(stream, null, options);
-      if (bitmap == null) {
-        // Treat null as an IO exception, we will eventually retry.
-        throw new IOException("Failed to decode stream.");
-      }
-      return bitmap;
-    }
   }
 }
